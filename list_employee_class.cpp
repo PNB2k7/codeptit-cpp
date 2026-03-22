bc2709ce1431f61ccbf9702ac0e15a1569e82fdc
PhiNguyenBinh <phinguyenbinh2k7@gmail.com>
2026-03-22 07:14:50 +0700

    CPP0614

---
+++ list_employee_class.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<iomanip>
+using namespace std;
+
+int n = 1;
+
+class Date{
+    private:
+        int day, month, year;
+    public:
+        Date(int day = 0, int month =0, int year =0) : day(day), month(month), year(year){}
+        friend istream& operator >> (istream &in, Date &a);
+        friend ostream& operator << (ostream &out, Date &a);
+
+};
+
+class NhanVien{
+    private:
+        string id, name, gender, locate, tax;
+        Date birth, contact;
+    public:
+        NhanVien(string id = "", string name = "", string gender = "", Date birth = Date(), string locate = "", string tax = "", Date contact = Date())
+            : id(id), name(name), gender(gender), birth(birth), locate(locate), tax(tax), contact(contact) {}
+        friend istream& operator >> (istream& in, NhanVien &a);
+        friend ostream& operator << (ostream& out, NhanVien &a);
+};
+
+istream& operator >> (istream &in, Date &a){
+    char slash;
+    in>>a.day>>slash>>a.month>>slash>>a.year;
+    return in;
+}
+ostream& operator << (ostream &out, Date &a){
+    out<<setw(2)<<setfill('0')<<a.day<<"/"
+    <<setw(2)<<setfill('0')<<a.month<<"/"
+    <<a.year;
+    return out;
+}
+istream& operator >> (istream &in, NhanVien &a){
+    string tmp = to_string(n++);
+    while(tmp.length() != 5) tmp.insert(tmp.begin(),'0');
+    a.id = tmp;
+    getline(in>>ws,a.name);
+    in>>a.gender>>a.birth;
+    getline(in>>ws,a.locate);
+    in>>a.tax>>a.contact;
+    return in;
+}
+ostream& operator << (ostream &out, NhanVien &a){
+    out<<a.id<<" "<<a.name<<" "<<a.gender<<" "<<a.birth<<" "<<a.locate<<" "<<a.tax<<" "<<a.contact<<endl;
+    return out;
+}
+
+int main(){
+    NhanVien ds[50];
+    int N,i;
+    cin >> N;
+    for(i=0;i<N;i++) cin >> ds[i];
+    for(i=0;i<N;i++) cout << ds[i];
+    return 0;
+}
