b16dc86b8cd229c2ee78cd4d655cb0d135de077e
PhiNguyenBinh <phinguyenbinh2k7@gmail.com>
2026-03-22 08:05:17 +0700

    CPP0615 - to AC, let compare year -> day -> month

--- list_employee_class.cpp
+++ list_employee_class.cpp
@@ -13,9 +13,9 @@ class Date{
         Date(int day = 0, int month =0, int year =0) : day(day), month(month), year(year){}
         friend istream& operator >> (istream &in, Date &a);
         friend ostream& operator << (ostream &out, Date &a);
+        int compare(Date a);
 
 };
-
 class NhanVien{
     private:
         string id, name, gender, locate, tax;
@@ -25,8 +25,14 @@ class NhanVien{
             : id(id), name(name), gender(gender), birth(birth), locate(locate), tax(tax), contact(contact) {}
         friend istream& operator >> (istream& in, NhanVien &a);
         friend ostream& operator << (ostream& out, NhanVien &a);
+        friend void sapxep(NhanVien ds[], int N);
 };
 
+int Date::compare(Date a){
+    if(a.year != year) return year < a.year;
+    if(a.month != month) return month < a.month;
+    return day < a.day;
+}
 istream& operator >> (istream &in, Date &a){
     char slash;
     in>>a.day>>slash>>a.month>>slash>>a.year;
@@ -52,12 +58,25 @@ ostream& operator << (ostream &out, NhanVien &a){
     out<<a.id<<" "<<a.name<<" "<<a.gender<<" "<<a.birth<<" "<<a.locate<<" "<<a.tax<<" "<<a.contact<<endl;
     return out;
 }
+void sapxep(NhanVien ds[], int N){
+    for(int i=0; i<N; i++){
+        int ok = 1;
+        for(int j=1; j<N-i; j++){
+            if(ds[j].birth.compare(ds[j-1].birth)){
+                swap(ds[j],ds[j-1]);
+                ok=0;
+            }
+        }
+        if(ok) return;
+    }
+}
 
 int main(){
     NhanVien ds[50];
     int N,i;
     cin >> N;
     for(i=0;i<N;i++) cin >> ds[i];
+    sapxep(ds, N);
     for(i=0;i<N;i++) cout << ds[i];
     return 0;
 }
